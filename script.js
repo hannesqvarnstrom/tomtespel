4bb37e5fb066cc14e539096748741affe41fb3bd
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2020-12-11 12:19:45 +0100

    moves into main script file

--- script.js
+++ script.js
@@ -27,3 +27,58 @@ const printGameBoard = () => {
   return arr;
 };
 //-----------------
+
+let santaPos = ""; //value of box where santa is
+const tomteString = `<div id="tomte"></div>`;
+const moveSanta = (target) => {
+  target.innerHTML = tomteString;
+  gameBoard.forEach((arr) =>
+    arr.forEach((position) => {
+      if (position.id == santaPos) {
+        position.innerHTML = "";
+      }
+    })
+  );
+  // find boardBox with id of id
+  // delete santa from where he is
+  // create santa in boardBox
+  santaPos = target.id;
+};
+
+const gameBoard = printGameBoard();
+
+const showLastPos = (pos) => {
+  gameBoard.forEach((arr) =>
+    arr.forEach((position) => {
+      if (position.id == pos) {
+        console.log(position);
+        position.classList.add("guessed");
+      }
+    })
+  );
+};
+gameBoard.forEach((arr) =>
+  arr.forEach((boardBox) => {
+    boardBox.addEventListener("click", (e) => {
+      showLastPos(santaPos);
+      moveSanta(e.target);
+      // const guess = reindeerCheck();
+      // if (guess) {
+      //   // korrekt gissning, öka något
+      // } else {
+      // fel gissning öka något annat
+      // }
+      // alert(e.target.id);
+    });
+  })
+);
+
+// gameBoard.forEach((square) => {
+//   square.forEach((innerSquare, j) => {
+//     let div = document.createElement("div");
+//     div.classList.add("boardBox");
+//     div.innerHTML = j;
+//     document.body.appendChild(div);
+//   });
+// });
+// ---------------------------
