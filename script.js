5779245f918cf7414bad1dd0203da3d504bb3253
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2020-12-11 12:00:05 +0100

    gameboard

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tomte = document.querySelector("#tomte");
 const ren1 = document.querySelector(".ren");
-
+// const gameBoard = document.querySelector("#gameBoard");
 // console.log(tomteCoords);
 // console.log(ren1Coords);
 // logik! ligger tomten inuti en div?
@@ -9,16 +9,17 @@ const ren1 = document.querySelector(".ren");
 const printGameBoard = () => {
   let arr = [];
   let gameBoard = document.createElement("div");
-
+  gameBoard.setAttribute("id", "gameBoard");
   for (let i = 0; i < 8; i++) {
     arr[i] = [];
     let container = document.createElement("div");
+    container.classList.add("row");
     gameBoard.appendChild(container);
     for (let j = 0; j < 8; j++) {
       let div = document.createElement("div");
       div.classList.add("boardBox");
       //   div.setAttribute("id");
-      div.setAttribute("value", `${i}` + `${j}`);
+      div.setAttribute("id", `${i}` + `${j}`);
       container.appendChild(div);
       arr[i][j] = div;
     }
